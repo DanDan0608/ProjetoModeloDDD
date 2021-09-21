22aa6821c66b654c4740b8ce9ee376ad1589e159
Daniel Katsuaki Esaka Souto Lino <daniel.lino@grupofleury.com.br>
2021-09-21 15:02:31 -0300

    5v - AutoMapper instalado e configurado no projeto

---
+++ ProjetoModeloDDDv/ProjetoModeloDDDv.MVC/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,27 @@
+﻿
+using AutoMapper;
+using ProjetoModeloDDD.MVC.AutoMapper;
+
+namespace ProjetoModeloDDD.MVC.AutoMapperConfig
+{
+    public class AutoMapperConfig
+    {
+        // config = new MapperConfiguration(cfg =>
+        // {
+        //  cfg.CreateMap<UsuarioViewModel, Models.Usuario>();
+        // });
+
+
+        public static void RegisterMappings()
+        {
+
+            Mapper.Initialize(x =>
+            {
+
+                x.AddProfile<DomainToViewModelMappingProfile>();
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+
+            });
+        }
+    }
+}
---
+++ ProjetoModeloDDDv/ProjetoModeloDDDv.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ProjetoModeloDDDv.Domain.Entities;
+using ProjetoModeloDDDv.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+
+        public override string ProfileName
+        {
+
+            get { return "ViewModelToDomainMappings"; }
+
+        }
+
+        protected override void Configure()
+        {
+
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+
+        }
+
+    }
+}
